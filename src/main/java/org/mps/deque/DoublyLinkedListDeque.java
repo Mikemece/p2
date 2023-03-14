77ba4876b3f6d1687c3ebade736728b8b26fde2a
MiguelMC <mcmiguel313@gmail.com>
2023-03-14 12:05:38 +0100

    sort y algun test

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -1,3 +1,4 @@
+
 package org.mps.deque;
 
 import java.util.Comparator;
@@ -17,57 +18,72 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
     @Override
     public void prepend(T value) {
         size++;
-        DequeNode<T> nodo = new DequeNode<T>(value, null, first);
-        if(first!=null){
-            first.setPrevious(nodo);
-            first = nodo;
-        }else{
-            first = nodo;
-            last = nodo;
+        DequeNode<T> node = new DequeNode<T>(value, null, null);
+        if (first == null) { // Lista vacía
+            first = node;
+            last = node;
+            // los punteros del nodo se quedan a null
+        } else { // lista no vacía
+            first.setPrevious(node);
+            node.setNext(first);
+            first = node;
         }
     }
 
     @Override
     public void append(T value) {
         size++;
-        DequeNode<T> nodo = new DequeNode<T>(value, last, null);
-        if(last!=null){
-            last.setNext(nodo);
-            last = nodo;
-        }else{
-            first = nodo;
-            last = nodo;
+        DequeNode<T> node = new DequeNode<T>(value, null, null);
+        if (first == null) { // Lista vacía
+            first = node;
+            last = node;
+            // los punteros del nodo se quedan a null
+        } else { // lista no vacía
+            last.setNext(node);
+            node.setPrevious(last);
+            last = node;
         }
-
     }
 
     @Override
     public void deleteFirst() {
-        if (first == null) {
+        if (first == null) { // lista vacía
             throw new DoubleEndedQueueException("Can't delete first element of empty list");
         }
-        DequeNode<T> next = first.getNext();
-        first = next;
-        if (first != null) first.setPrevious(null);
+        size--;
+        if (first == last) { // lista con un elemento
+            first = null;
+            last = null;
+        } else { // lista con mas de un elemento
+            first = first.getNext();
+            first.setPrevious(null);
+        }
     }
 
     @Override
     public void deleteLast() {
-        if (last == null) {
+        if (last == null) { // lista vacía
             throw new DoubleEndedQueueException("Can't delete last element of empty list");
         }
-        DequeNode<T> prev = last.getPrevious();
-        last = prev;
-        if (last != null) last.setNext(null);
+        size--;
+        if (first == last) { // lista con un elemento
+            first = null;
+            last = null;
+        } else { // lista con mas de un elemento
+            last = last.getPrevious();
+            last.setNext(null);
+        }
     }
 
     @Override
     public T first() {
+        if (first == null) return null;
         return first.getItem();
     }
 
     @Override
     public T last() {
+        if (last == null) return null;
         return last.getItem();
     }
 
@@ -76,9 +92,23 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         return size;
     }
 
+    public boolean contains(T value) {
+        DequeNode<T> current = first;
+        while (current != null) {
+            if (current.getItem().equals(value)) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
     @Override
     public T get(int index) {
         DequeNode<T> nodo= first;
+        if(nodo==null){
+            throw new DoubleEndedQueueException("List is empty");
+        }
         while(index>0){
             nodo = nodo.getNext();
             index--;
@@ -88,13 +118,6 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         }
         return  nodo.getItem();
     }
-
-    @Override
-    public boolean contains(T value) {
-
-        return false;
-    }
-
     @Override
     public void remove(T value) {
         DequeNode<T> nodo= first;
@@ -112,6 +135,30 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
 
     @Override
     public void sort(Comparator<? super T> comparator) {
+        DoublyLinkedListDeque<T> listaRes = new DoublyLinkedListDeque<T>();
+        T value;
+        int i = size;
+        while(size>0){
+            value = minimo(comparator);
+            listaRes.append(value);
+            remove(value);
+            i--;
+        }
+        this.first = listaRes.first;
+        this.last = listaRes.last;
+    }
 
+    private T minimo(Comparator<? super T> comparator){
+        int i = size;
+        DequeNode<T> nodo = first;
+        T minimo = first.getItem();
+        while(i > 0){
+            if(comparator.compare(nodo.getItem(), nodo.getNext().getItem())>0){
+                minimo = nodo.getNext().getItem();
+            }
+            nodo = nodo.getNext();
+            i--;
+        }
+        return minimo;
     }
 }
